d4079dd0f9711f23f39ad430f8a1cb6603844746
Gabe <gabedominguez@outlook.com>
2013-09-10 16:43:36 -0700

    completed simple todo angular app

---
+++ main.rb
@@ -0,0 +1,20 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sinatra/activerecord'
+require 'haml'
+
+get "/" do
+  haml :index, :format => :html5
+end
+
+get '/towersofhanoi' do
+  haml :towersofhanoi, :format => :html5
+end
+
+get "/japanese" do
+  haml :japanese, :format => :html5
+end
+
+get "/clouds" do
+  haml :index, :format => :html5
+end
